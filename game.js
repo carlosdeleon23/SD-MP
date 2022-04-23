0c067e84864ca3863aa268770ad6342fbb92f51d
Carlos De Leon <79179033+carlosdeleon23@users.noreply.github.com>
2022-04-22 21:19:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,6 @@
-var character = document.getElementById("character");
-document.addEventListener("click",jump);
-function jump(){
-    if(character.classList == "animate"){return;}
-    character.classList.add("animate");
-    setTimeout(removeJump,300); //300ms = length of animation
-};
-function removeJump(){
- 
-    character.classList.remove("animate");
-}
-
-var block = document.getElementById("block");
-
-function checkDead(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        alert("Game over");
+const grid = document.querySelector('.grid)
+                                
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div)
+    grid.appendChild(square)
     }
-}
-
-setInterval(checkDead, 10);
